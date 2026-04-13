e494c42773142fde59902194d1950a5ff2f39356
Christian P <73241408+Cannon7206@users.noreply.github.com>
2026-04-12 23:58:30 -0400

    finished part 4

--- HW7/HW7-4.X/MAIN.asm
+++ HW7/HW7-4.X/MAIN.asm
@@ -1,60 +1,63 @@
 #include "p16f84a.inc"
 
-; CONFIG
-; __config 0xFFF9
- __CONFIG _FOSC_XT & _WDTE_OFF & _PWRTE_OFF & _CP_OFF
+    __CONFIG _FOSC_XT & _WDTE_OFF & _PWRTE_OFF & _CP_OFF
 
-CBLOCK
+CBLOCK 0x0C
     REDLED
     GREENLED
 ENDC
-    
-    ORG	    0X00
+
+    ORG     0x00
     GOTO    SETUP
-    ORG	    0X04
+    ORG     0x04
     RETFIE
-    
+
+;--------------------------------------------------
 SETPORT
-    BSF	    STATUS, RP0
-    CLRF    TRISA
-    MOVLW   0XFF
-    MOVWF   TRISB
-    MOVLW   0X00
-    BCF	    STATUS, RP0
-    
+    BSF     STATUS, RP0     ; Bank 1
+    CLRF    TRISA           ; PORTA all outputs
+    MOVLW   0xFF
+    MOVWF   TRISB           ; PORTB all inputs
+    BCF     STATUS, RP0     ; Bank 0
+    RETURN
+
+;--------------------------------------------------
 SETUP
     CALL    SETPORT
     CLRF    PORTA
     MOVLW   0x01
-    MOVWF   REDLED
-    MOVLW   0X02
-    MOVWF   GREENLED
+    MOVWF   REDLED          ; RA0 = Red LED
+    MOVLW   0x02
+    MOVWF   GREENLED        ; RA1 = Green LED
     GOTO    MAIN
-    
-    
-MAIN
-    MOVF    PORTB, W
-    SUBLW   D'10'
-    BTFSC   STATUS, C
-    GOTO    RED
-    GOTO    GREEN
 
-RED
-    CALL    REDLEDON
-    GOTO    MAIN
-   
-GREEN
+;--------------------------------------------------
+; AA
+MAIN
+    MOVF    PORTB, W        ; Read weight
+    ADDLW   D'245'          ; Carry set if PORTB >= 11 (WEIGHT > 10)
+    BTFSC   STATUS, C
+    GOTO    GREEN           ; Yes -> GREENON -> BB
+    GOTO    RED             ; No  -> REDON  -> BB
+
+;--------------------------------------------------
+GREEN                       ; GREENON
     CALL    GREENLEDON
-    GOTO    MAIN
-    
-    
-    
+    GOTO    MAIN            ; BB -> back to AA
+
+RED                         ; REDON
+    CALL    REDLEDON
+    GOTO    MAIN            ; BB -> back to AA
+
+;--------------------------------------------------
 REDLEDON
     MOVF    REDLED, W
     MOVWF   PORTA
     RETURN
-    
+
 GREENLEDON
     MOVF    GREENLED, W
     MOVWF   PORTA
     RETURN
+
+    END
---
+++ HW7/HW7-4.X/build/default/production/MAIN.o.d
@@ -0,0 +1,3 @@
+build/default/production/MAIN.o:  \
+	D:/school/CPEG208/HW7/HW7-4.X/MAIN.asm  \
+
