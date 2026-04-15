bc99914a810d0d5955639426a3e13763469c64fb
Christian P <Awesomelegoguy76@gmail.com>
2026-04-15 16:19:12 -0400

    Update MAIN.asm

--- HW11/HW1.X/MAIN.asm
+++ HW11/HW1.X/MAIN.asm
@@ -1,9 +1,9 @@
 #include "p16f84a.inc"
-__CONFIG _FOSC_HS & _WDTE_OFF & _PWRTE_OFF & _CP_OFF
+ __CONFIG _FOSC_HS & _WDTE_OFF & _PWRTE_OFF & _CP_OFF
 
-CBLOCK	0X0C
+ CBLOCK	    0X0C
     POWER
-ENDC
+ ENDC
 
     ORG	    0X00
     GOTO    SETUP
@@ -20,3 +20,73 @@ SETPORT
     MOVWF   TRISA
     CLRF    TRISB
     BCF	    STATUS, RP0
+    RETURN
+    
+MAIN
+    CALL    ON_OFF
+    BTFSS   POWER, 0
+    GOTO    MAIN
+    CALL    BOX_SIZE
+    GOTO    MAIN
+    
+ON_OFF
+    BTFSC   PORTA, 0
+    CALL    MOTOR_ON
+    CALL    MOTOR_OFF
+    RETURN
+    
+MOTOR_ON
+    BSF	    PORTB, 0
+    MOVLW   0X01
+    MOVWF   POWER
+    RETURN
+    
+MOTOR_OFF
+    BCF	    PORTB, 0
+    CLRF    POWER
+    RETURN
+    
+BOX_SIZE
+    BTFSC   PORTA, 1
+    GOTO    BOX_SIZE_1
+    CALL    NO_BOX
+    RETURN
+BOX_SIZE_1
+    BTFSC   PORTA, 2
+    GOTO    BOX_SIZE_2
+    CALL    SMALL_BOX
+    RETURN
+BOX_SIZE_2
+    BTFSC   PORTA, 3
+    GOTO    BOX_SIZE_3
+    CALL    MEDIUM_BOX
+    RETURN
+BOX_SIZE_3
+    CALL    LARGE_BOX
+    RETURN
+    
+NO_BOX
+    BCF	    PORTB, 1
+    BCF	    PORTB, 2
+    BCF	    PORTB, 3
+    RETURN
+    
+SMALL_BOX
+    BSF	    PORTB, 1
+    BCF	    PORTB, 2
+    BCF	    PORTB, 3
+    RETURN
+
+MEDIUM_BOX
+    BCF	    PORTB, 1
+    BSF	    PORTB, 2
+    BCF	    PORTB, 3
+    RETURN
+    
+LARGE_BOX
+    BCF	    PORTB, 1
+    BCF	    PORTB, 2
+    BCF	    PORTB, 3
+    RETURN
+    
+    END
